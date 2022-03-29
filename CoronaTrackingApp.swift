7cdcbe972fbf79d1c1a3a047778b7a724d3f5523
WANG WENJUAN <45557662+wenjuanW-ds@users.noreply.github.com>
2022-03-29 14:07:56 +0200

    Add files via upload

---
+++ CoronaTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaTrackingApp.swift
+//  CoronaTracking
+//
+//  Created by wenjuan wang on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoronaTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
