69b995ea06d9c8839007aecde7b75a05704aefd2
WANG WENJUAN <45557662+wenjuanW-ds@users.noreply.github.com>
2022-03-29 14:08:33 +0200

    Add files via upload

---
+++ FavorisView.swift
@@ -0,0 +1,21 @@
+//
+//  FavorisView.swift
+//  CoronaTracking
+//
+//  Created by wenjuan wang on 20/03/2021.
+//
+
+import SwiftUI
+
+
+struct FavorisView: View {
+    var body: some View {
+        Text("favrois page")
+    }
+}
+
+struct FavorisView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavorisView()
+    }
+}
